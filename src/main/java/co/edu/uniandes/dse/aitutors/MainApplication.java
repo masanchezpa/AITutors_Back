bfb2474b84a7e3122353aa35546372c726f10775
estudiante <estudiante@ISIS2603-202420.sis.virtual.uniandes.edu.co>
2024-08-12 07:23:33 -0500

    Fix error in maven

--- src/main/java/co/edu/uniandes/dse/aitutors/MainApplication.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/MainApplication.java
@@ -1,4 +1,4 @@
-package co.edu.uniandes.dse.ZZZ;
+package co.edu.uniandes.dse.aitutors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
