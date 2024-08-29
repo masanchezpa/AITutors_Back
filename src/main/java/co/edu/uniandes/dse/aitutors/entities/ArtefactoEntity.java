244d12768ab850ca63e157489f989634497ef96e
FLACKIUS <>
2024-08-29 07:57:24 -0500

    implementacion para javax, junto con ArtefactoRepository

--- src/main/java/co/edu/uniandes/dse/aitutors/entities/ArtefactoEntity.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/entities/ArtefactoEntity.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -23,8 +23,9 @@ import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
-public  class AretefactoEntity extends BaseEntity {
+public  class ArtefactoEntity extends BaseEntity {
 
+	
 	private String tipo;
 	private String contenido;
 	private UsuarioEntity autor; 
