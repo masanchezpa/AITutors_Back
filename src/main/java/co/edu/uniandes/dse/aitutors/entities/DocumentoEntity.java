84d5881780bee937b19312decfc49e615be6a597
FLACKIUS <>
2024-08-29 11:06:26 -0500

    Entidad documento no tenia el @Entity

--- src/main/java/co/edu/uniandes/dse/aitutors/entities/DocumentoEntity.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/entities/DocumentoEntity.java
@@ -1,5 +1,9 @@
 package co.edu.uniandes.dse.aitutors.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
@@ -7,8 +11,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+
+import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
 
+
+@Data
+@Entity
 public class DocumentoEntity extends BaseEntity {
 
     private String tipo;
