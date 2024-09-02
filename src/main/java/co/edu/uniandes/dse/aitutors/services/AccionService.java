db874128e8af99217de29a20c575df90b18e1f5e
FLACKIUS <>
2024-09-02 10:41:17 -0500

    Logica de accion, lista para pruebas

--- src/main/java/co/edu/uniandes/dse/aitutors/services/AccionService.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/services/AccionService.java
@@ -1,6 +1,8 @@
 package co.edu.uniandes.dse.aitutors.services;
 
 
+import static org.mockito.ArgumentMatchers.anyChar;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +30,45 @@ public class AccionService {
         log.info("Creating a new accion");
         return accionRepository.save(accion);
     }
+
+    @Transactional
+    public List<AccionEntity> getAcciones() {
+        log.info("Retrieving all acciones");
+        return accionRepository.findAll();
+    }
+
+
+    @Transactional
+    public AccionEntity getAccion(Long accionId) throws EntityNotFoundException {
+        log.info("Retrieving the accion with id = {0}", accionId);
+        Optional<AccionEntity> accionEntity = accionRepository.findById(accionId);
+        if (accionEntity.isEmpty()) {
+            throw new EntityNotFoundException("There is no accion with id = " + accionId);
+        }
+        return accionEntity.get();
+    }
+
+    @Transactional
+    public AccionEntity updateAccion(Long accionId, AccionEntity accion) throws EntityNotFoundException {
+        log.info("Updating the accion with id = {0}", accionId);
+        Optional<AccionEntity> accionEntity = accionRepository.findById(accionId);
+        if (!accionEntity.isPresent()) {
+            throw new EntityNotFoundException("There is no accion with id = " + accionId);
+        }
+        return accionRepository.save(accion);
+    }
+
+    @Transactional
+    public void deleteAccion(Long accionId) throws EntityNotFoundException {
+        log.info("Deleting the accion with id = {0}", accionId);
+        Optional<AccionEntity> accion = accionRepository.findById(accionId);
+        if (!accion.isPresent()) {
+            throw new EntityNotFoundException("There is no accion with id = " + accionId);
+        }
+        accionRepository.deleteById(accionId);
+    }
+
+
 
 
 }
