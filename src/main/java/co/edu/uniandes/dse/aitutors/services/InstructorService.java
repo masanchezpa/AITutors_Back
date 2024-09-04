d40734f7221cb40e940a96c9ac57cc47416f31a3
FLACKIUS <>
2024-09-03 19:23:45 -0500

    Todas mis entidades de logica implementadas

---
+++ src/main/java/co/edu/uniandes/dse/aitutors/services/InstructorService.java
@@ -0,0 +1,69 @@
+package co.edu.uniandes.dse.aitutors.services;
+
+
+import static org.mockito.ArgumentMatchers.anyChar;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import co.edu.uniandes.dse.aitutors.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.aitutors.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.aitutors.entities.InstructorEntity;
+import co.edu.uniandes.dse.aitutors.repositories.InstructorRepository;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Slf4j
+@Service
+public class InstructorService {
+    
+    @Autowired
+    InstructorRepository instructorRepository;
+
+    @Transactional
+    public InstructorEntity createInstructor(InstructorEntity instructor) {
+        log.info("Creating a new instructor");
+        return instructorRepository.save(instructor);
+    }
+
+    @Transactional
+    public InstructorEntity getInstructor(Long instructorId) throws EntityNotFoundException {
+        log.info("Retrieving the instructor with id = {0}", instructorId);
+        Optional<InstructorEntity> instructorEntity = instructorRepository.findById(instructorId);
+        if (instructorEntity.isEmpty()) {
+            throw new EntityNotFoundException("There is no instructor with id = " + instructorId);
+        }
+        return instructorEntity.get();
+    }
+
+    @Transactional
+    public List<InstructorEntity> getInstructors() {
+        log.info("Retrieving all instructors");
+        return instructorRepository.findAll();
+    }
+
+    @Transactional
+    public InstructorEntity updateInstructor(Long instructorId, InstructorEntity instructor) throws EntityNotFoundException {
+        log.info("Updating the instructor with id = {0}", instructorId);
+        Optional<InstructorEntity> instructorEntity = instructorRepository.findById(instructorId);
+        if (!instructorEntity.isPresent()) {
+            throw new EntityNotFoundException("There is no instructor with id = " + instructorId);
+        }
+        return instructorRepository.save(instructor);
+    }
+
+    @Transactional
+    public void deleteInstructor(Long instructorId) throws EntityNotFoundException {
+        log.info("Deleting the instructor with id = {0}", instructorId);
+        Optional<InstructorEntity> instructorEntity = instructorRepository.findById(instructorId);
+        if (!instructorEntity.isPresent()) {
+            throw new EntityNotFoundException("There is no instructor with id = " + instructorId);
+        }
+        instructorRepository.deleteById(instructorId);
+    }
+
+}
