e90a1359fa2d3e9826ca1d8b598f8f5de94bc0d9
Felipe Gaitan <141869493+Flackiusv2@users.noreply.github.com>
2024-09-08 13:59:25 -0500

    Update EntityNotFoundException.java

--- src/main/java/co/edu/uniandes/dse/aitutors/exceptions/EntityNotFoundException.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/exceptions/EntityNotFoundException.java
@@ -30,13 +30,6 @@ public class EntityNotFoundException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 	
-	//public EntityNotFoundException(String message) {
-	//	super(message);
-	//}
-
-	public EntityNotFoundException() {
-        super();
-    }
 
 	public EntityNotFoundException(String message) {
         super(message);
