d45266f35b8c858ba008daec274d304feea43665
Daniel Bolivar <5009484+danielbolivar@users.noreply.github.com>
2024-09-16 23:02:02 -0500

    fixed: typo

--- src/main/java/co/edu/uniandes/dse/aitutors/dto/TemaDTO.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/dto/TemaDTO.java
@@ -1,5 +1,11 @@
 package co.edu.uniandes.dse.aitutors.dto;
 
+import java.util.Optional;
+
+import lombok.Data;
+
+@Data
 public class TemaDTO {
-    
+    private String titulo;
+    private String descripcion;
 }
