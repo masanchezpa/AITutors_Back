b4d4b669df812704db921d2239ae47d4a178578e
Flackiusv2 <141869493+Flackiusv2@users.noreply.github.com>
2024-09-25 13:10:55 -0500

    sonarQube fix

--- src/test/java/co/edu/uniandes/dse/aitutors/services/ComentarioUsuarioServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/aitutors/services/ComentarioUsuarioServiceTest.java
@@ -16,105 +16,88 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 import co.edu.uniandes.dse.aitutors.entities.ComentarioEntity;
 import co.edu.uniandes.dse.aitutors.entities.UsuarioEntity;
 import co.edu.uniandes.dse.aitutors.exceptions.EntityNotFoundException;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
-/**
- * Pruebas de logica de la relacion Prize - Author
- *
- * @author ISIS2603
- */
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @Transactional
 @Import(ComentarioUsuarioService.class)
 class ComentarioUsuarioServiceTest {
 
-	private PodamFactory factory = new PodamFactoryImpl();
+    private PodamFactory factory = new PodamFactoryImpl();
 
-	@Autowired
-	private ComentarioUsuarioService comentarioUsuarioService;
+    @Autowired
+    private ComentarioUsuarioService comentarioUsuarioService;
 
-	@Autowired
-	private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
-	private List<UsuarioEntity> usuarioList = new ArrayList<>();
-	private List<ComentarioEntity> comentarioList = new ArrayList<>();
+    private List<ComentarioEntity> comentarioList = new ArrayList<>();
+    private List<UsuarioEntity> usuarioList = new ArrayList<>();
 
-	/**
-	 * Configuración inicial de la prueba.
-	 */
-	@BeforeEach
-	void setUp() {
-		clearData();
-		insertData();
-	}
-
-	/**
-	 * Limpia las tablas que están implicadas en la prueba.
-	 */
-	private void clearData() {
-		entityManager.getEntityManager().createQuery("delete from ComentarioEntity").executeUpdate();
-		entityManager.getEntityManager().createQuery("delete from UsuarioEntity").executeUpdate();
-	}
-
-	/**
-	 * Inserta los datos iniciales para el correcto funcionamiento de las pruebas.
-	 */
-
-    private void insertData(){
-        for (int i = 0; i < 3; i++) {
-            UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
-            entityManager.persist(entity);
-            usuarioList.add(entity);
-        }
-        for (int i = 0; i < 3; i++) {
-            ComentarioEntity entity = factory.manufacturePojo(ComentarioEntity.class);
-            entityManager.persist(entity);
-            comentarioList.add(entity);
-            if (i == 0) {
-                entity.setAutor(usuarioList.get(0));
-            }
-        }
+    @BeforeEach
+    void setUp() {
+        clearData();
+        insertData();
     }
-    
+
+    private void clearData() {
+        entityManager.getEntityManager().createQuery("delete from ComentarioEntity").executeUpdate();
+        entityManager.getEntityManager().createQuery("delete from UsuarioEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        for (int i = 0; i < 2; i++) {
+            ComentarioEntity comentario = factory.manufacturePojo(ComentarioEntity.class);
+            entityManager.persist(comentario);
+            comentarioList.add(comentario);
+        }
+        for (int i = 0; i < 2; i++) {
+            UsuarioEntity usuario = factory.manufacturePojo(UsuarioEntity.class);
+            entityManager.persist(usuario);
+            usuarioList.add(usuario);
+        }
+        comentarioList.get(0).setAutor(usuarioList.get(0));
+        entityManager.persist(comentarioList.get(0));
+    }
+
     @Test
     void testAddUsuario() throws EntityNotFoundException {
-        UsuarioEntity entity = usuarioList.get(0);
-        ComentarioEntity comentarioEntity = comentarioList.get(1);
-        UsuarioEntity response = comentarioUsuarioService.addUsuario( comentarioEntity.getId(), entity.getId());
+        ComentarioEntity comentario = comentarioList.get(1);
+        UsuarioEntity usuario = usuarioList.get(1);
+        UsuarioEntity response = comentarioUsuarioService.addUsuario(comentario.getId(), usuario.getId());
         assertNotNull(response);
-        assertEquals(entity.getId(), response.getId());
+        assertEquals(usuario.getId(), response.getId());
     }
-	
-	@Test
+
+    @Test
     void testAddInvalidUsuario() {
         assertThrows(EntityNotFoundException.class, () -> {
-            ComentarioEntity comentarioEntity = comentarioList.get(1);
-            comentarioUsuarioService.addUsuario(0L, comentarioEntity.getId());
+            ComentarioEntity comentario = comentarioList.get(1);
+            comentarioUsuarioService.addUsuario(comentario.getId(), 0L);
         });
     }
-	
+
     @Test
     void testAddUsuarioInvalidComentario() {
         assertThrows(EntityNotFoundException.class, () -> {
-            UsuarioEntity entity = usuarioList.get(0);
-            comentarioUsuarioService.addUsuario(entity.getId(), 0L);
+            UsuarioEntity usuario = usuarioList.get(0);
+            comentarioUsuarioService.addUsuario(0L, usuario.getId());
         });
     }
 
-	@Test
+    @Test
     void testGetUsuario() throws EntityNotFoundException {
-        ComentarioEntity entity = comentarioList.get(0);
-        UsuarioEntity response = comentarioUsuarioService.getUsuario(entity.getId());
+        ComentarioEntity comentario = comentarioList.get(0);
+        UsuarioEntity response = comentarioUsuarioService.getUsuario(comentario.getId());
         assertNotNull(response);
-        assertEquals(entity.getAutor().getId(), response.getId());
+        assertEquals(comentario.getAutor().getId(), response.getId());
     }
-	
+
     @Test
     void testGetUsuarioInvalidComentario() {
         assertThrows(EntityNotFoundException.class, () -> {
@@ -122,52 +105,33 @@ class ComentarioUsuarioServiceTest {
         });
     }
 
-    
-
-	@Test
+    @Test
     void testReplaceUsuario() throws EntityNotFoundException {
-        UsuarioEntity entity = usuarioList.get(0);
-        ComentarioEntity comentarioEntity = comentarioList.get(1);
-        UsuarioEntity response = comentarioUsuarioService.replaceUsuario(comentarioEntity.getId(), entity.getId() );
+        UsuarioEntity usuario = usuarioList.get(0);
+        ComentarioEntity comentario = comentarioList.get(1);
+        UsuarioEntity response = comentarioUsuarioService.replaceUsuario(comentario.getId(), usuario.getId());
         assertNotNull(response);
-        assertEquals(entity.getId(), response.getId());
-    }
-
-	
-	@Test
-	void testReplaceInvalidUsuario() {
-        assertThrows(EntityNotFoundException.class, () -> {
-            comentarioUsuarioService.replaceUsuario(0L, comentarioList.get(1).getId());
-        });
-    }
-
-
-	@Test
-	void testReplaceComentarioInvalidComentario() {
-        assertThrows(EntityNotFoundException.class, () -> {
-            comentarioUsuarioService.replaceUsuario(usuarioList.get(0).getId(), 0L);
-        });
-    }
-	
-    @Test
-    void testRemoveComentario() throws EntityNotFoundException {
-        comentarioUsuarioService.removeComentario(usuarioList.get(0).getId());
-        ComentarioEntity response = comentarioUsuarioService.getComentario(usuarioList.get(0).getId());
-        assertNull(response);
+        assertEquals(usuario.getId(), response.getId());
     }
 
     @Test
-    void testRemoveInvalidComentario() {
+    void testReplaceInvalidUsuario() {
+        assertThrows(EntityNotFoundException.class, () -> {
+            comentarioUsuarioService.replaceUsuario(0L, usuarioList.get(1).getId());
+        });
+    }
+
+    @Test
+    void testRemoveUsuario() throws EntityNotFoundException {
+        comentarioUsuarioService.removeUsuario(comentarioList.get(0).getId());
+        ComentarioEntity comentario = entityManager.find(ComentarioEntity.class, comentarioList.get(0).getId());
+        assertNull(comentario.getAutor());
+    }
+
+    @Test
+    void testRemoveInvalidUsuario() {
         assertThrows(EntityNotFoundException.class, () -> {
             comentarioUsuarioService.removeUsuario(0L);
         });
     }
-
-	@Test
-    void testRemoveUsuario() throws EntityNotFoundException {
-        comentarioUsuarioService.removeUsuario(comentarioList.get(0).getId());
-        UsuarioEntity response = comentarioUsuarioService.getUsuario(comentarioList.get(0).getId());
-        assertNull(response);
-    }
-
 }
