92368cb8ba36e8546463f4ecd7c7a566431d432b
juanardila10 <js.ardilal1@uniandes.edu.co>
2024-09-29 01:07:51 -0500

    Uncomment Verifications

--- src/main/java/co/edu/uniandes/dse/aitutors/services/DocumentoService.java
+++ src/main/java/co/edu/uniandes/dse/aitutors/services/DocumentoService.java
@@ -35,17 +35,17 @@ public class DocumentoService {
             throw new IllegalOperationException("El contenido del documento no puede ser vacío");
         }
 
-        // if (documentoEntity.getTema()==null){
-        //     throw new IllegalOperationException("El tema del documento no puede ser nulo");
-        // }
+        if (documentoEntity.getTema()==null){
+            throw new IllegalOperationException("El tema del documento no puede ser nulo");
+        }
         
-        // Optional<TemaEntity> temaEntity = temaRepository.findById(documentoEntity.getTema().getId());
+        Optional<TemaEntity> temaEntity = temaRepository.findById(documentoEntity.getTema().getId());
 
-        // if (temaEntity.isEmpty()){
-        //     throw new IllegalOperationException("El tema del documento no existe");
-        // }
+        if (temaEntity.isEmpty()){
+            throw new IllegalOperationException("El tema del documento no existe");
+        }
 
-        // documentoEntity.setTema(temaEntity.get());
+        documentoEntity.setTema(temaEntity.get());
 
         log.info("Termina proceso de creación del documento");
         return documentoRepository.save(documentoEntity);
